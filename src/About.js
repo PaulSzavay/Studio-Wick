af6ea27ce73b78fc3a88fe71774411296d73ca9f
Paul Szavay <szavay.paul@gmail.com>
2024-01-08 15:50:18 -0500

    more paragraph changes

--- src/About.js
+++ src/About.js
@@ -130,7 +130,7 @@ font-size: 2rem;
 margin: 0;
 
 @media only screen and  (max-width: 1000px) {
-font-size: 1.1rem;
+font-size: 1.5rem;
 text-align: center;
 }
 
@@ -148,7 +148,7 @@ text-indent: 0%;
 text-align: center;
 font-size: 0.85rem;
 margin: 0rem;
-
+padding-top: 1rem;
 }
 `
 
