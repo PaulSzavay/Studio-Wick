627e2c593ad91019a29fbab8506ea13e109ae8f1
Paul Szavay <szavay.paul@gmail.com>
2024-01-08 15:22:34 -0500

    Updated about section

--- src/About.js
+++ src/About.js
@@ -1,21 +1,55 @@
 import { styled } from "styled-components";
-import Steph from "./Assets/StockSteph2.JPG"
-
+import Steph1 from "./Assets/Steff2Update.jpg"
+import Steph2 from "./Assets/Steff1Update.jpg"
+import Steph3 from "./Assets/Steff3Update.jpg"
 
 const About = () => {
 
     return(
         <>
         <AboutSection>
+        <Box>
+            <Section>
+            <PhotoDiv>
+            <WickPhoto src={Steph1}/>
+            </PhotoDiv>
+            <AboutText>
+                <AboutTitle>Who is Steff?</AboutTitle>
+                <AboutDescription>Steff always wanted to be a hairstylist. Graduating from Algonquin College in Hairstyling, Steff spent the first 6 years of her career in Ottawa. In 2021, she moved back home to Montreal, and in 2023, opened StudioWick. Steff specializes in precision cutting and takes a lot of pride in it. She also has a passion for highlighting and big colour transformations.</AboutDescription>
+            </AboutText>
+            </Section>
+
+            <Section2>
+            <AboutText>
+                <AboutTitle>What makes Steff different?</AboutTitle>
+                <AboutDescription>Steff is motivated by challenging society's beauty standards, and strives to create an environment where people don't have to conform to a specific image. She believes in listening to her clients and offering her opinions in a way that complements their desires.</AboutDescription>
+            </AboutText>
+            <PhotoDiv>
+            <WickPhoto src={Steph2}/>
+            </PhotoDiv>
+            </Section2>
+
+            <Section>
+            <PhotoDiv>
+            <WickPhoto src={Steph3}/>
+            </PhotoDiv>
+            <AboutText>
+                <AboutTitle>Steff's Goal</AboutTitle>
+                <AboutDescription>When clients visit StudioWick, they can expect a safe space where they can truly be themselves. Steff is dedicated to listening, understanding, and making your hair goals a reality. She aims to provide an atmosphere that fosters a sense of comfort and belonging.</AboutDescription>
+            </AboutText>
+            </Section>
+{/*             
             <PhotoDiv>
             <WickPhoto src={Steph}/>
             </PhotoDiv>
             <AboutText>
                 <AboutTitle>Who is Steff?</AboutTitle>
-                <AboutDescriptionTop>Steff always wanted to be a hairstylist. Graduating from Algonquin College in Hairstyling, Steff spent the first 6 years of her career in Ottawa. In 2021, she moved back home to Montreal, and in 2023, opened StudioWick. Steff specializes in precision cutting and takes a lot of pride in it. She also has a passion for highlighting and big colour transformations.</AboutDescriptionTop>
+                {/* <AboutDescriptionTop>Steff always wanted to be a hairstylist. Graduating from Algonquin College in Hairstyling, Steff spent the first 6 years of her career in Ottawa. In 2021, she moved back home to Montreal, and in 2023, opened StudioWick. Steff specializes in precision cutting and takes a lot of pride in it. She also has a passion for highlighting and big colour transformations.</AboutDescriptionTop>
                 <AboutDescription>Steff is motivated by challenging society's beauty standards, and strives to create an environment where people don't have to conform to a specific image. She believes in listening to her clients and offering her opinions in a way that complements their desires.</AboutDescription>
-                <AboutDescription>When clients visit StudioWick, they can expect a safe space where they can truly be themselves. Steff is dedicated to listening, understanding, and making your hair goals a reality. She aims to provide an atmosphere that fosters a sense of comfort and belonging.</AboutDescription>
-            </AboutText>
+                <AboutDescription>When clients visit StudioWick, they can expect a safe space where they can truly be themselves. Steff is dedicated to listening, understanding, and making your hair goals a reality. She aims to provide an atmosphere that fosters a sense of comfort and belonging.</AboutDescription> */}
+            {/* </AboutText> */}
+
+            </Box>
         </AboutSection>
 
         </>
@@ -26,56 +60,83 @@ export default About
 
 
 const AboutSection = styled.section`
+background-color: #C6B8AF;
 display: flex;
-flex-direction: row;
 justify-content: center;
 align-items: center;
-height: 80vh;
-width: 100%;
-background-color: #C6B8AF;
-box-sizing: border-box;
 `
+
+
+const Box = styled.div`
+max-width: 1100px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const Section = styled.div`
+min-height: 175px;
+display: flex;
+justify-content: center;
+align-items: center;
+@media only screen and  (max-width: 1000px) {
+    flex-direction: column-reverse;
+}
+`
+
+const Section2 = styled(Section)`
+@media only screen and  (max-width: 1000px) {
+    flex-direction: column;
+}
+`
+
+
 
 const PhotoDiv = styled.div`
 display: flex;
-justify-content: end;
+justify-content: center;
 align-items: center;
-width: 50%;
 padding: 1rem;
 `
 
 const WickPhoto = styled.img`
 object-fit: contain;
-height: 65vh;
+border-radius: 25%;
+width: 200px;
 `
 
 const AboutText = styled.div`
-width: 50%;
+
 display: flex;
 flex-direction: column;
 padding-left: 3rem;
+padding-top: 1.5rem;
+
+@media only screen and  (max-width: 1000px) {
+    padding-left: 1rem;
+    font-size: smaller;
+}
+@media only screen and  (max-width: 300px) {
+    padding-left: 0.5rem;
+    font-size: x-small;
+}
 `
 
 const AboutTitle = styled.h1`
-font-size: 3rem;
-margin: 0 ;
+/* font-size: 3rem;
+margin: 0; */
+
 `
 
 const AboutDescription = styled.p`
-width: 43rem;
+/* width: 43rem;
 font-size: 1.5rem;
 text-indent: 50px;
 text-align: justify;
-text-justify: inter-word;
+text-justify: inter-word; */
 `
 
-const AboutDescriptionTop = styled.p`
-width: 43rem;
-font-size: 1.5rem;
-margin-top: 0 ;
-text-indent: 50px;
-text-align: justify;
-text-justify: inter-word;
-`
+
 
 
