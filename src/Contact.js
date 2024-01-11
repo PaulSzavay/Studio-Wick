35b2e605c7d1d4868c65c85e0586cf3e726f8606
Paul Szavay <szavay.paul@gmail.com>
2024-01-11 14:28:42 -0500

    contact form responsiveness added

--- src/Contact.js
+++ src/Contact.js
@@ -42,9 +42,22 @@ align-items: center;
 `
 
 const ContactTitle = styled.h1`
+
+@media only screen and (max-width: 350px) {
+    text-align: center;
+    font-size: 1.5rem;
+  }
 `
 
 const ContactRequest = styled.p`
+@media only screen and (max-width: 750px) {
+    text-align: center;
+    padding: 0rem 2rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    padding: 0rem 1rem;
+  }
 `
 
 const ContactInformation = styled.div`
@@ -63,6 +76,15 @@ align-items: center;
 const Name = styled.label`
 position: relative;
 left: -13.25rem;
+
+@media only screen and (max-width: 750px) {
+    left: -8rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    left: -5.9rem;
+    font-size: 0.9rem;
+  }
 `
 
 const NameInput = styled.input`
@@ -70,11 +92,29 @@ const NameInput = styled.input`
   padding: 12px 20px;
   margin: 8px 0;
   font-family: 'Montserrat', sans-serif;
+
+  @media only screen and (max-width: 750px) {
+    width: 20rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 15rem;
+    margin: 4px 0;
+  }
 `
 
 const Email = styled.label`
 position: relative;
 left: -13.4rem;
+
+@media only screen and (max-width: 750px) {
+    left: -8.25rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    left: -6.1rem;
+    font-size: 0.9rem;
+  }
 `
 
 const EmailInput = styled.input`
@@ -82,11 +122,29 @@ const EmailInput = styled.input`
   padding: 12px 20px;
   margin: 8px 0;
   font-family: 'Montserrat', sans-serif;
+
+  @media only screen and (max-width: 750px) {
+    width: 20rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 15rem;
+    margin: 4px 0;
+  }
 `
 
 const Subject = styled.label`
 position: relative;
 left: -12.75rem;
+
+@media only screen and (max-width: 750px) {
+    left: -7.75rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    left: -5.6rem;
+    font-size: 0.9rem;
+  }
 `
 
 const SubjectInput = styled.input`
@@ -94,20 +152,75 @@ const SubjectInput = styled.input`
   padding: 12px 20px;
   margin: 8px 0;
   font-family: 'Montserrat', sans-serif;
+
+  @media only screen and (max-width: 750px) {
+    width: 20rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 15rem;
+    margin: 4px 0;
+  }
 `
 
 const Message = styled.label`
 position: relative;
 left: -12.55rem;
+
+@media only screen and (max-width: 750px) {
+    left: -7.5rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    left: -5.4rem;
+    font-size: 0.9rem;
+  }
 `
 
 const MessageInput = styled.textarea`
   width: 30rem;
+  height: 15rem;
   padding: 12px 20px;
   margin: 8px 0;
   font-family: 'Montserrat', sans-serif;
+  
+  @media only screen and (max-width: 750px) {
+    width: 20rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 15rem;
+    margin: 4px 0;
+  }
 `
 
 const Submit = styled.button`
+font-family: 'Montserrat', sans-serif;
 margin-top: 1rem;
+width: 50%;
+height: 4rem;
+font-size: 2rem;
+border-radius: 1rem;
+background-color: #212e1a;
+color: #9fa37f;
+border: 0.1rem solid #9fa37f;
+transition: 0.2s ease-in-out;
+
+@media only screen and (max-width: 750px) {
+    width: 65%;
+    height: 3rem;
+    font-size: 1.5rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 50%;
+    height: 2.25rem;
+    font-size: 1.2rem;
+  }
+
+&:hover{
+    background-color: #9fa37f;
+    color: #212e1a;
+    border: 0.1rem solid #212e1a;
+}
 `
